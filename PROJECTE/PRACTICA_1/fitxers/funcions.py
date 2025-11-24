11aafb4bd589a1a8c95a8559630dfc93f611379f
rgarci06 <rgarci@insdanielblanxart.cat>
2025-11-24 16:30:14 +0100

    GOD

--- PROJECTE/PRACTICA_1/fitxers/funcions.py
+++ PROJECTE/PRACTICA_1/fitxers/funcions.py
@@ -1,24 +1,13 @@
 import pandas as pd
 import os
 
-# funció per fer dataFrame ----------------------------------------------------------------------
+def obrir_fitxer(nom_carpeta: str, fitxer: str):
+    """Obre un fitxer CSV i el retorna com a DataFrame."""
+    
+    ruta = os.path.join(os.path.dirname(__file__), nom_carpeta, fitxer)
 
-def obrir_fitxer(nom_carpeta:str, fitxer:str):
-    """Funció per buscar fitxer i transformar-lo en DataFrame
-
-    Args:
-        nom_carpeta (str): carpeta
-        fitxer (str): fitxer csv que volem transformar
-
-    Returns:
-        DataFrame o 0 en cas d'error
-    """
-    DIRECTORI=os.path.dirname(__file__)
-
-    RUTA=os.path.join(DIRECTORI, nom_carpeta, fitxer)
-    try:
-        df=pd.read_csv(RUTA)
-        return df
-    except FileNotFoundError:
+    if not os.path.exists(ruta):
         print("Error: Fitxer no trobat")
         return 0
+
+    return pd.read_csv(ruta)
